49ec1575b2e028de38ad84a11dd39e593c5a1020
jp <jpc.araujo@hotmail.com>
2019-02-08 17:04:51 -0300

    Favorites, sort notes when favoriting,status bar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+Vue.filter('date', time => moment(time).format('DD/MM/YY, HH:mm'))
+
 new Vue({
     el: '#app',
     data(){
@@ -15,6 +17,37 @@ new Vue({
         },
         selectedNote(){
             return this.notes.find(note => note.id === this.selectedId)
+        },
+        sortedNotes(){
+            return this.notes.slice()
+                .sort((a,b) => a.created - b.created)
+                .sort((a,b) => (a.favorite === b.favorite) ? 0
+                : a.favorite ? -1
+                : 1)
+        },
+        linesCount(){
+            if(this.selectedNote){
+                //Conta o número de linhas
+                return this.selectedNote.content.split(/\r\n|\r|\n/).length
+            }
+        },
+        wordsCount(){
+            if(this.selectedNote){
+                var s = this.selectedNote.content
+                //Transforma quebra de linhas em espaços em branco
+                s = s.replace(/\n/g,' ')
+                //Exclui espacos iniciais e finas (trim)
+                s = s.replace(/(^\s*) | (\s*$)/gi,'')
+                //Tranforma 2 ou mais espaços em branco em 1
+                s = s.replace(/\s\s+/gi,' ')
+                //Retorna o número de espaços
+                return s.split(' ').length
+            }
+        },
+        charactersCount(){
+            if(this.selectedNote){
+                return this.selectedNote.content.split('').length
+            }
         }
     },
     methods:{
@@ -24,7 +57,7 @@ new Vue({
             const note = {
                 id: String(time),
                 title: `Nova anotação ${this.notes.length + 1}`,
-                content: '**Olá!** Essa anotação usa [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) na sua formatação!',
+                content: '**Olá!** Essa anotação usa [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) em sua formatação!',
                 created: time,
                 favorite: false
             }
@@ -49,6 +82,10 @@ new Vue({
                     this.notes.splice(index,1)
                 }
             }
+        },
+        favoriteNote(){
+            this.selectedNote.favorite ^= true
+            console.log(this.selectedNote)
         }
     },
     watch:{
